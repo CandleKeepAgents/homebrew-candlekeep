f0927578bad270c17d3ce5b548b3ddba0859c324
Sahar Carmel <augu144@gmail.com>
2026-02-16 12:35:24 +0200

    fix: point formula URLs to homebrew-candlekeep releases (fixes 404)

--- Formula/candlekeep-cli.rb
+++ Formula/candlekeep-cli.rb
@@ -6,11 +6,11 @@ class CandlekeepCli < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/CandleKeepAgents/candlekeep/releases/download/cli-v0.7.0/ck-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/CandleKeepAgents/homebrew-candlekeep/releases/download/v0.7.0/ck-aarch64-apple-darwin.tar.gz"
       sha256 "db1e24b422a1dd13c70acf30d5e03707ef0552c84e4c570436a398cf9e4e6ee3"
     end
     on_intel do
-      url "https://github.com/CandleKeepAgents/candlekeep/releases/download/cli-v0.7.0/ck-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/CandleKeepAgents/homebrew-candlekeep/releases/download/v0.7.0/ck-x86_64-apple-darwin.tar.gz"
       sha256 "5ffbef347ee2007c2cd308a53190de74b90ba905b56901af41171d9dc7e551ec"
     end
   end
